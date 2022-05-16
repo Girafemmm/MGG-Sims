a8c6ec0d9efef9001bb434f38a4235b5dad89b75
huang-jinlu <girafe6h@gmail.com>
2022-05-16 18:02:34 +0800

    界面更新

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,29 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-
+    statusData:[
+      {
+        label:'现金',
+        value:'10000',
+      }
+    ],
+    attributeData:[
+      {
+        label:'健康',
+        value: 70,
+        color:'rgb(245,108,108)'
+      },
+      {
+        label:'精力',
+        value: 70,
+        color:'rgb(64,158,255)'
+      },
+      {
+        label:'心情',
+        value: 70,
+        color:'rgb(103,194,58)'
+      }
+    ]
   },
   mutations: {
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -4,11 +4,27 @@
       <el-col :span="6">
         <div class="index-left">
           <div>
+            <div>资产</div>
+            <el-table
+                :data="statusData"
+                style="width: 100%"
+                :show-header="false">
+              <el-table-column
+                  prop="label"
+                  width="50">
+              </el-table-column>
+              <el-table-column
+                  prop="value"
+              >
+              </el-table-column>
+            </el-table>
+          </div>
+          <div>
             <div>状态</div>
             <div class="user_status">
               <template>
                 <el-table
-                    :data="statusData"
+                    :data="attributeData"
                     style="width: 100%"
                     :show-header="false">
                   <el-table-column
@@ -36,7 +52,25 @@
       </el-col>
       <el-col :span="12">
         <div class="index-center">
-
+          <el-tabs v-model="activeTab" @tab-click="handleClickTab">
+            <el-tab-pane label="角色" name="character">
+              <el-button plain>发传单</el-button>
+            </el-tab-pane>
+            <el-tab-pane label="学习" name="study">
+              <el-button plain>数学</el-button>
+              <el-button plain>文学</el-button>
+              <el-button plain>美术</el-button>
+              <el-button plain>音乐</el-button>
+              <el-button plain>计算机</el-button>
+              <el-button plain>金融</el-button>
+            </el-tab-pane>
+            <el-tab-pane label="商店" name="shop">
+              商店
+            </el-tab-pane>
+            <el-tab-pane label="设置" name="setting">
+              设置
+            </el-tab-pane>
+          </el-tabs>
         </div>
       </el-col>
       <el-col :span="6">
@@ -55,30 +89,23 @@ export default {
   name: "index",
   data(){
     return{
-      statusData:[
-        {
-          label:'健康',
-          value:'70',
-          color:'rgb(245,108,108)'
-        },
-        {
-          label:'精力',
-          value:'70',
-          color:'rgb(64,158,255)'
-        },
-        {
-          label:'心情',
-          value:'70',
-          color:'rgb(103,194,58)'
-        }
-      ]
+      statusData:[],
+      attributeData:[],
+      activeTab:'character',
+
     }
   },
   mounted() {
-
+    this.init()
   },
   methods:{
-
+    init(){
+      this.statusData = this.$store.state.statusData
+      this.attributeData = this.$store.state.attributeData
+    },
+    handleClickTab(tab, event) {
+      console.log(tab, event);
+    }
   }
 }
 </script>
